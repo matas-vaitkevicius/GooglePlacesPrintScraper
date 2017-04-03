4ae6e11bbb2baabbcb35d82b7602c21de1cf1e66
liufa <mataswork@gmail.com>
2017-04-03 10:25:48 +0700

    Added mechanism to construct Url added API key to configuration

--- GooglePlacesPrintScraper/PrintScraper.cs
+++ GooglePlacesPrintScraper/PrintScraper.cs
@@ -13,15 +13,15 @@ namespace GooglePlacesPrintScraper
     {
         static void Main(string[] args)
         {
-            var keywords = ConfigurationManager.AppSettings.Get("keywords").Split(new[] {','});
-
+            var keywords ="(" + string.Join(") OR (", ConfigurationManager.AppSettings.Get("keywords").Split(new[] {','}))+")";
+            var googlePlacesApiKey = ConfigurationManager.AppSettings.Get("googlePlacesApiKey");
             var locationsToBeSearched = File.ReadAllLines("../../../data/us_postal_codes.csv");
 
             foreach (var locationTobeSearched in locationsToBeSearched)
             {
                 try
                 {
-                    CallGooglePlacesAPIAndSetCallback(locationTobeSearched, keywords);
+                    CallGooglePlacesAPIAndSetCallback(locationTobeSearched, keywords, googlePlacesApiKey);
                 }
                 catch (Exception e)
                 {
@@ -31,7 +31,7 @@ namespace GooglePlacesPrintScraper
 
         }
 
-        public static async void CallGooglePlacesAPIAndSetCallback(string location, string[] keywords)
+        public static async void CallGooglePlacesAPIAndSetCallback(string location, string keywords, string googlePlacesApiKey)
         {
             var latitudeAndLongitude = location.Split(new[] { ',' }).Skip(5).ToList();
 
@@ -40,10 +40,10 @@ namespace GooglePlacesPrintScraper
 
             if (decimal.TryParse(latitudeAndLongitude[0], out latitude) && decimal.TryParse(latitudeAndLongitude[1], out longitude))
             {
-
+                
                 using (var client = new HttpClient())
                 {
-                    var response = await client.GetStringAsync(string.Format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={0},{1}&radius=500&type=bar&key=YourAPIKey", latitude, longitude));
+                    var response = await client.GetStringAsync(string.Format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={0},{1}&radius=500&keyword={2}&key={3}", latitude, longitude,keywords, googlePlacesApiKey));
                     dynamic result = response;
                 }
             }
